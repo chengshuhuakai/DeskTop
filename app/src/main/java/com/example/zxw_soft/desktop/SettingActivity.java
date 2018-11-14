9e2c523aaf71fda345ea94488c6a775bdc44e8cf
zxw_soft <519609821@qq.com>
2018-11-14 11:53:24 +0800

    進行了SettingActivity 的滾動佈局以及初步切換。

--- app/src/main/java/com/example/zxw_soft/desktop/SettingActivity.java
+++ app/src/main/java/com/example/zxw_soft/desktop/SettingActivity.java
@@ -8,20 +8,34 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.text.LoginFilter;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SettingActivity extends FragmentActivity  {
 
-    public ImageButton mBt_boda;
-    public ImageButton mBt_tonghuajilu;
-    public ImageButton mBt_dianhuaben;
-    public ImageButton mBt_lanyashebei;
+    public String TAG ="SETTING" ;
+    private RadioButton mBtnBackLight;
+    private RadioButton mBtnFactorySetting;
+    private RadioButton mBtnNav;
+    private RadioButton mBtnSteeringWheel;
+    private RadioButton mBtnMore;
+    private RadioButton mBtnGps;
+    private RadioButton mBtnTime;
+    private RadioButton mBtnSystemSetting;
+    private RadioButton mBtnSystemMessage;
+    private RadioButton mBtnEffectSetting;
+    private RadioButton mBtnLanguage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,36 +47,74 @@ public class SettingActivity extends FragmentActivity  {
         initView();
     }
 
-    private void initView() {
-        mBt_boda = (ImageButton) findViewById(R.id.BtnDial);
-        mBt_tonghuajilu = (ImageButton) findViewById(R.id.BtnCallLog);
-        mBt_dianhuaben = (ImageButton) findViewById(R.id.BtnTelBook);
-        mBt_lanyashebei = (ImageButton) findViewById(R.id.BtnBluetoothtools);
+     void initView() {
+     /*   mBtnBackLight=(RadioButton)findViewById(R.id.BtnBackLight);
+        mBtnFactorySetting=(RadioButton)findViewById(R.id.BtnFactorySetting);
+        mBtnNav=(RadioButton)findViewById(R.id.BtnNav);
+        mBtnSteeringWheel=(RadioButton)findViewById(R.id.BtnSteeringWheel);
+        mBtnMore=(RadioButton)findViewById(R.id.BtnMore);
+        mBtnGps=(RadioButton)findViewById(R.id.BtnGPS);
+        mBtnTime=(RadioButton)findViewById(R.id.BtnTime);
+        mBtnSystemSetting=(RadioButton)findViewById(R.id.BtnSystemSetting);
+        mBtnSystemMessage=(RadioButton)findViewById(R.id.BtnSystemMessage);
+        mBtnEffectSetting=(RadioButton)findViewById(R.id.BtnEffectSetting);
+        mBtnLanguage=(RadioButton)findViewById(R.id.BtnLanguage);*/
+        int[] buttons =new int[]{R.id.BtnBackLight,R.id.BtnFactorySetting,R.id.BtnNav,R.id.BtnSteeringWheel,R.id.BtnMore,R.id.BtnGPS,R.id.BtnTime,R.id.BtnSystemSetting,R.id.BtnSystemMessage,R.id.BtnEffectSetting,R.id.BtnLanguage};
+        RadioButton[] buttonsName= new RadioButton[]{mBtnBackLight,mBtnFactorySetting,mBtnNav,mBtnSteeringWheel,mBtnMore,mBtnGps,mBtnTime,mBtnSystemSetting,mBtnSystemMessage,mBtnEffectSetting,mBtnLanguage};
+        for (int i = 0; i <buttons.length ; i++) {
+                buttonsName[i]= (RadioButton) findViewById(buttons[i]);
+                buttonsName[i].setOnClickListener(new ButtonListener());
+                 //Log.d(TAG, "initView:"+ buttonsName[i]);
+        }
 
-        mBt_boda.setOnClickListener(new ButtonListener());
-        mBt_tonghuajilu.setOnClickListener(new ButtonListener());
-        mBt_dianhuaben.setOnClickListener(new ButtonListener());
-        mBt_lanyashebei.setOnClickListener(new ButtonListener());
        // replaceRightFragment(new BodaFragment());
     }
     class ButtonListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
             switch (v.getId()){
-                case R.id.BtnDial:
-                    Toast.makeText(getApplicationContext(),"bodaFragment创建了",Toast.LENGTH_LONG).show();
+                case R.id.BtnBackLight:
+                    Toast.makeText(getApplicationContext(),"BackLightFragment创建了",Toast.LENGTH_LONG).show();
                     replaceRightFragment(new BodaFragment());
                     break;
-                case R.id.BtnCallLog:
-                    Toast.makeText(getApplicationContext(),"TonghuajiluFragment创建了",Toast.LENGTH_LONG).show();
+                case R.id.BtnFactorySetting:
+                    Toast.makeText(getApplicationContext(),"FactorySettingFragment创建了",Toast.LENGTH_LONG).show();
                     replaceRightFragment(new TongHuaJiLuFragment());
                     break;
-                case R.id.BtnTelBook:
-                    Toast.makeText(getApplicationContext(),"dianhuabenFragment创建了",Toast.LENGTH_LONG).show();
+                case R.id.BtnNav:
+                    Toast.makeText(getApplicationContext(),"NavFragment创建了",Toast.LENGTH_LONG).show();
                     replaceRightFragment(new DianHuaBenFragment());
                     break;
-                case R.id.BtnBluetoothtools:
+                case R.id.BtnSteeringWheel:
+                    Toast.makeText(getApplicationContext(),"SteeringWheelFragment创建了",Toast.LENGTH_LONG).show();
+                    replaceRightFragment(new LanYaSheBeiFragment());
+                    break;
+                case R.id.BtnMore:
+                    Toast.makeText(getApplicationContext(),"MoreFragment创建了",Toast.LENGTH_LONG).show();
+                    replaceRightFragment(new LanYaSheBeiFragment());
+                    break;
+                case R.id.BtnGPS:
+                    Toast.makeText(getApplicationContext(),"GPSFragment创建了",Toast.LENGTH_LONG).show();
+                    replaceRightFragment(new LanYaSheBeiFragment());
+                    break;
+                case R.id.BtnTime:
+                    Toast.makeText(getApplicationContext(),"TimeFragment创建了",Toast.LENGTH_LONG).show();
+                    replaceRightFragment(new LanYaSheBeiFragment());
+                    break;
+                case R.id.BtnSystemSetting:
                     Toast.makeText(getApplicationContext(),"lanyashebeiFragment创建了",Toast.LENGTH_LONG).show();
+                    replaceRightFragment(new LanYaSheBeiFragment());
+                    break;
+                case R.id.BtnSystemMessage:
+                    Toast.makeText(getApplicationContext(),"SystemSettingFragment创建了",Toast.LENGTH_LONG).show();
+                    replaceRightFragment(new LanYaSheBeiFragment());
+                    break;
+                case R.id.BtnEffectSetting:
+                    Toast.makeText(getApplicationContext(),"EffectSettingFragment创建了",Toast.LENGTH_LONG).show();
+                    replaceRightFragment(new LanYaSheBeiFragment());
+                    break;
+                case R.id.BtnLanguage:
+                    Toast.makeText(getApplicationContext(),"LanguageFragment创建了",Toast.LENGTH_LONG).show();
                     replaceRightFragment(new LanYaSheBeiFragment());
                     break;
 
