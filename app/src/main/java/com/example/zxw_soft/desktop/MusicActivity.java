8355ebfe46f75f39e2384cb6af4be52591f34262
zxw_soft <519609821@qq.com>
2018-11-15 16:06:33 +0800

    設置了MusicAcitivity 以及簡單設置了它的界面2 ，計劃設置一個後台播放音樂的播放器。

--- app/src/main/java/com/example/zxw_soft/desktop/MusicActivity.java
+++ app/src/main/java/com/example/zxw_soft/desktop/MusicActivity.java
@@ -1,14 +1,34 @@
 package com.example.zxw_soft.desktop;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
 import android.graphics.Color;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.app.Activity;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import java.io.IOException;
+import java.net.URL;
+
 public class MusicActivity extends Activity {
+
+    /**
+     * 规定开始音乐、暂停音乐、结束音乐的标志
+     */
+    public  static final int PLAT_MUSIC=1;
+    public  static final int PAUSE_MUSIC=2;
+    public  static final int STOP_MUSIC=3;
+
+    private MediaPlayer mMediaPlayer;
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +37,17 @@ public class MusicActivity extends Activity {
         //setFullScreen(this);
         transparencyStatusBar(this);
         setContentView(R.layout.activity_music);
+        initView();
+        init();
     }
+
+    private void init() {
+
+    }
+    private void initView() {
+        //TODO 设置监听事件
+    }
+
     private static void transparencyStatusBar(Activity activity) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             //5.0及其以上
